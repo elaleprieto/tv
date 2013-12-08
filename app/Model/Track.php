76f09f26efba40c7faebfce47c53223838e76917
Ale Prieto <aleprieto@gmail.com>
2013-12-08 14:02:01 -0300

    Se agrega el usuario al track

--- app/Model/Track.php
+++ app/Model/Track.php
@@ -60,6 +60,22 @@ class Track extends AppModel {
 		)
 	);
 
+	public $belongsTo = array(
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
 
 /**
  * hasAndBelongsToMany associations
